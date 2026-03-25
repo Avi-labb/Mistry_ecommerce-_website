2cdfacec30e5e9e1b984475ceba46c4fe21e4ce6
Avi <avidevelop60@gmail.com>
2026-03-25 13:15:18 +0530

    server chnages

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mistry-shop-xo.onrender.com",
+  baseURL: "https://mistry-ship-xo.onrender.com",
   withCredentials: true,
 });
 
