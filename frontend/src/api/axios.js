409f23c78f5e4019d37974de8c88a83f21c72e82
Avinash Kushwaha <avidevelop60@gmail.com>
2026-04-02 02:59:00 +0530

    Update API base URL in axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mistry-ship-xo.onrender.com",
+  baseURL: "https://mistry-shop-xox.onrender.com",
   withCredentials: true,
 });
 
